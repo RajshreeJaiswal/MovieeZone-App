26ec5fdabf40f59b7a25145f257351f12c0a2724
RajsheeJaiswal <rajshreejaiswal0907@gmail.com>
2024-11-14 11:38:58 +0530

    Resolved merge conflicts

--- themeContext.jsx
+++ themeContext.jsx
@@ -19,4 +19,24 @@ export const ThemeProvider =({children}) =>{
         </ThemeContext.Provider>
     );
 
+};import { createContext, useContext, useState } from "react";
+
+const ThemeContext = createContext();
+
+export const useTheme = () => useContext(ThemeContext);
+
+export const ThemeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      <div className={darkMode ? "dark-mode" : "light-mode"}>
+        {children}
+      </div>
+    </ThemeContext.Provider>
+  );
 };
